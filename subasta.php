883dd6038877520eae326c31bd9d9afb34ceed74
franciscohreyes <francisco.hdezreyes@gmail.com>
2017-01-20 09:27:09 -0500

    Modificaciones al archivo subasta.php, main.html y footer.html - se agrego el archivo style-timeline.css - se agrego el archivo main.js

--- subasta.php
+++ subasta.php
@@ -15,7 +15,7 @@
 						<li class="tab"><a href="#tab7">PRÓXIMAMENTE</a></li>
 					</ul>
 				</div>
-				<div id="tab2" class="col s12">Test 1</div>
+				
 				<div id="tab3" class="col s12">Test 3</div>
 				<div id="tab4" class="col s12">Test 4</div>
 				<div id="tab5" class="col s12">Test 5</div>
@@ -49,6 +49,153 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- <div id="tab2" class="col s12">
+		<div class="container no-pad-top" style="padding-left: 3vw;">
+			<div class="row">
+				<div class="col l12">
+					<h5 style="font-size: 2.1em; padding-top: 9vw; font-weight: 500">
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+					</h5>
+				</div>
+				<div class="col l7">
+					<div class="col l5" style="float: right;; padding-top: 4vw;">
+						<h5 class="light">1. Concepto</h5>
+						<p style="line-height: 140%">
+							Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> -->
+
+	<div id="tab2" class="col s12">
+		<section class="cd-horizontal-timeline">
+			<div class="events-content">
+				<ol>
+					<li class="selected" data-date="16/01/2014">
+						<h2>
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+						</h2>
+						<div class="row">
+							<div class="col l7">
+								<div class="col l5" style="float: right;; padding-top: 4vw;">
+									<h5 class="light">1. Concepto</h5>
+									<p>
+										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li data-date="28/02/2014">
+						<h2>
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+						</h2>
+						<div class="row">
+							<div class="col l7">
+								<div class="col l5" style="float: right;; padding-top: 4vw;">
+									<h5 class="light">2. Sueños</h5>
+									<p>
+										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li data-date="20/04/2014">
+						<h2>
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+						</h2>
+						<div class="row">
+							<div class="col l7">
+								<div class="col l5" style="float: right;; padding-top: 4vw;">
+									<h5 class="light">3. Magia</h5>
+									<p>
+										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li data-date="20/05/2014">
+						<h2>
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+						</h2>
+						<div class="row">
+							<div class="col l7">
+								<div class="col l5" style="float: right;; padding-top: 4vw;">
+									<h5 class="light">4. Pasión</h5>
+									<p>
+										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+					<li data-date="09/07/2014">
+						<h2>
+						Todo comienza con los sueños, <br>
+						es el punto de partida a un viaje interno.
+						</h2>
+						<div class="row">
+							<div class="col l7">
+								<div class="col l5" style="float: right;; padding-top: 4vw;">
+									<h5 class="light">5. Luz</h5>
+									<p>
+										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+									</p>
+								</div>
+							</div>
+						</div>
+					</li>
+
+				</ol>
+			</div> 
+			<!-- .events-content -->
+
+			<!-- Start .timeline -->
+			<div class="timeline">
+				<!-- Start .events-wrapper -->
+				<div class="events-wrapper">
+					<div class="events">
+						<ol>
+							<li><a href="#0" data-date="16/01/2014" class="selected">CONCEPTO</a></li>
+							<li><a href="#0" data-date="28/02/2014">SUEÑOS</a></li>
+							<li><a href="#0" data-date="20/04/2014">MAGIA</a></li>
+							<li><a href="#0" data-date="20/05/2014">PASIÓN</a></li>
+							<li><a href="#0" data-date="09/07/2014">LUZ</a></li>
+							<li><a href="#0" data-date="30/08/2014"></a></li>
+							<li><a href="#0" data-date="15/09/2014"></a></li>
+							<li><a href="#0" data-date="01/11/2014"></a></li>
+							<li><a href="#0" data-date="10/12/2014"></a></li>
+							<li><a href="#0" data-date="19/01/2015"></a></li>
+							<li><a href="#0" data-date="03/03/2015"></a></li>
+						</ol>
+						<span class="filling-line" aria-hidden="true"></span>
+					</div> <!-- .events -->
+				</div> <!-- End .events-wrapper -->
+				
+				<!-- Start .cd-timeline-navigation -->
+				<!-- <ul class="cd-timeline-navigation">
+					<li><a href="#0" class="prev inactive">Prev</a></li>
+					<li><a href="#0" class="next">Next</a></li>
+				</ul>  --> <!-- End .cd-timeline-navigation -->
+			</div> <!-- End .timeline -->
+		</section>
+	</div>
+
+	
 
 <?php include_once("templates/footer.html");?>
 <script src="js/esculturas.js"></script>

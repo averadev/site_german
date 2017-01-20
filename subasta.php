f1e0d3cfcf78478e536bb04a7a79a9e1bae6a729
franciscohreyes <fhernandez@geekbucket.com.mx>
2017-01-20 18:15:35 -0500

    Modificaciones en subasta.php - timeline proceso - modificaciones en style-timeline.css

--- subasta.php
+++ subasta.php
@@ -25,7 +25,7 @@
 		</div>
 	</div>
 
-	<div id="tab1" class="bg_proceso" >
+	<div id="tab1" class="bg_porque" >
 		<div class="container no-pad-top" style="padding-left: 3vw;">
 			<div class="row">
 				<div class="col l12">
@@ -71,128 +71,142 @@
 		</div>
 	</div> -->
 
-	<div id="tab2" class="col s12">
-		<section class="cd-horizontal-timeline">
-			<div class="events-content">
-				<ol>
-					<li class="selected" data-date="16/01/2014">
-						<h2>
-						Todo comienza con los sueños, <br>
-						es el punto de partida a un viaje interno.
-						</h2>
-						<div class="row">
-							<div class="col l7">
-								<div class="col l5" style="float: right;; padding-top: 4vw;">
-									<h5 class="light">1. Concepto</h5>
-									<p>
-										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
-									</p>
-								</div>
-							</div>
-						</div>
-					</li>
+	<div id="tab2" class="bgg_proceso">
+		<div class="container no-pad-top" style="padding-left: 3vw;">
+			<div class="row">
+				<div class="col l12">
+					<section class="cd-horizontal-timeline">
+						<div class="events-content">
+							<ol>
+								<li class="selected" data-date="16/01/2014">
+									<h2>
+									Todo comienza con los sueños, <br>
+									es el punto de partida a un viaje interno.
+									</h2>
+									<div class="row">
+										<div class="col l4">
+      										<img src="media/img/subasta/bg_concepto.png" width="250" height="250" alt="" class="img_proceso">
+										</div>
+										<div class="col l3" style="padding-top: 4vw;">
+											<h5 class="light">1. Concepto</h5>
+											<p>
+												Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+											</p>
+										</div>
+										<div class="col l5">
+      										<img src="media/img/subasta/bg_concepto_2.png" width="591" height="600" alt="" class="img_proceso_2">
+										</div>
+									</div>
+								</li>
 
-					<li data-date="28/02/2014">
-						<h2>
-						Todo comienza con los sueños, <br>
-						es el punto de partida a un viaje interno.
-						</h2>
-						<div class="row">
-							<div class="col l7">
-								<div class="col l5" style="float: right;; padding-top: 4vw;">
-									<h5 class="light">2. Sueños</h5>
-									<p>
-										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
-									</p>
-								</div>
-							</div>
-						</div>
-					</li>
+								<li data-date="28/02/2014">
+									<h2>
+									Todo comienza con los sueños, <br>
+									es el punto de partida a un viaje interno.
+									</h2>
+									<div class="row">
+										<div class="col l4">
+      										<img src="media/img/subasta/bg_concepto.png" width="250" height="250" alt="" class="img_proceso">
+										</div>
+										<div class="col l3" style="padding-top: 4vw;">
+											<h5 class="light">2. Sueños</h5>
+											<p>
+												Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+											</p>
+										</div>
+										<div class="col l5">
+      										<img src="media/img/subasta/bg_suenos.png" width="485" height="485" alt="" class="img_proceso_3">
+										</div>
+									</div>
+								</li>
 
-					<li data-date="20/04/2014">
-						<h2>
-						Todo comienza con los sueños, <br>
-						es el punto de partida a un viaje interno.
-						</h2>
-						<div class="row">
-							<div class="col l7">
-								<div class="col l5" style="float: right;; padding-top: 4vw;">
-									<h5 class="light">3. Magia</h5>
-									<p>
-										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
-									</p>
-								</div>
-							</div>
-						</div>
-					</li>
+								<li data-date="20/04/2014">
+									<h2>
+									Todo comienza con los sueños, <br>
+									es el punto de partida a un viaje interno.
+									</h2>
+									<div class="row">
+										<div class="col l7">
+											<div class="col l5" style="float: right; padding-top: 4vw;">
+												<h5 class="light">3. Magia</h5>
+												<p>
+													Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+												</p>
+											</div>
+										</div>
+									</div>
+								</li>
 
-					<li data-date="20/05/2014">
-						<h2>
-						Todo comienza con los sueños, <br>
-						es el punto de partida a un viaje interno.
-						</h2>
-						<div class="row">
-							<div class="col l7">
-								<div class="col l5" style="float: right;; padding-top: 4vw;">
-									<h5 class="light">4. Pasión</h5>
-									<p>
-										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
-									</p>
-								</div>
-							</div>
-						</div>
-					</li>
+								<li data-date="20/05/2014">
+									<h2>
+									Todo comienza con los sueños, <br>
+									es el punto de partida a un viaje interno.
+									</h2>
+									<div class="row">
+										<div class="col l7">
+											<div class="col l5" style="float: right; padding-top: 4vw;">
+												<h5 class="light">4. Pasión</h5>
+												<p>
+													Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+												</p>
+											</div>
+										</div>
+									</div>
+								</li>
 
-					<li data-date="09/07/2014">
-						<h2>
-						Todo comienza con los sueños, <br>
-						es el punto de partida a un viaje interno.
-						</h2>
-						<div class="row">
-							<div class="col l7">
-								<div class="col l5" style="float: right;; padding-top: 4vw;">
-									<h5 class="light">5. Luz</h5>
-									<p>
-										Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
-									</p>
-								</div>
-							</div>
-						</div>
-					</li>
+								<li data-date="09/07/2014">
+									<h2>
+									Todo comienza con los sueños, <br>
+									es el punto de partida a un viaje interno.
+									</h2>
+									<div class="row">
+										<div class="col l7">
+											<div class="col l5" style="float: right; padding-top: 4vw;">
+												<h5 class="light">5. Luz</h5>
+												<p>
+													Esta obra esta hecha con polizethano mundo donde la naturaleza, el misticismo, la magia, el erotismo, el dolor y la pasión entrelazan para dar forma material. Rey gordo de España, fue exhumado y trasladado en una caja de zinc que pesó un kilo.
+												</p>
+											</div>
+										</div>
+									</div>
+								</li>
 
-				</ol>
-			</div> 
-			<!-- .events-content -->
+							</ol>
+						</div> 
+						<!-- .events-content -->
 
-			<!-- Start .timeline -->
-			<div class="timeline">
-				<!-- Start .events-wrapper -->
-				<div class="events-wrapper">
-					<div class="events">
-						<ol>
-							<li><a href="#0" data-date="16/01/2014" class="selected">CONCEPTO</a></li>
-							<li><a href="#0" data-date="28/02/2014">SUEÑOS</a></li>
-							<li><a href="#0" data-date="20/04/2014">MAGIA</a></li>
-							<li><a href="#0" data-date="20/05/2014">PASIÓN</a></li>
-							<li><a href="#0" data-date="09/07/2014">LUZ</a></li>
-							<li><a href="#0" data-date="30/08/2014"></a></li>
-							<li><a href="#0" data-date="15/09/2014"></a></li>
-							<li><a href="#0" data-date="01/11/2014"></a></li>
-							<li><a href="#0" data-date="10/12/2014"></a></li>
-							<li><a href="#0" data-date="19/01/2015"></a></li>
-							<li><a href="#0" data-date="03/03/2015"></a></li>
-						</ol>
-						<span class="filling-line" aria-hidden="true"></span>
-					</div> <!-- .events -->
-				</div> <!-- End .events-wrapper -->
-				
-				<!-- Start .cd-timeline-navigation -->
-				<!-- <ul class="cd-timeline-navigation">
-					<li><a href="#0" class="prev inactive">Prev</a></li>
-					<li><a href="#0" class="next">Next</a></li>
-				</ul>  --> <!-- End .cd-timeline-navigation -->
-			</div> <!-- End .timeline -->
-		</section>
+						<!-- Start .timeline -->
+						<div class="timeline">
+							<!-- Start .events-wrapper -->
+							<div class="events-wrapper">
+								<div class="events">
+									<ol>
+										<li><a href="#0" data-date="16/01/2014" class="selected">CONCEPTO</a></li>
+										<li><a href="#0" data-date="28/02/2014">SUEÑOS</a></li>
+										<li><a href="#0" data-date="20/04/2014">MAGIA</a></li>
+										<li><a href="#0" data-date="20/05/2014">PASIÓN</a></li>
+										<li><a href="#0" data-date="09/07/2014">LUZ</a></li>
+										<li><a href="#0" data-date="30/08/2014"></a></li>
+										<li><a href="#0" data-date="15/09/2014"></a></li>
+										<li><a href="#0" data-date="01/11/2014"></a></li>
+										<li><a href="#0" data-date="10/12/2014"></a></li>
+										<li><a href="#0" data-date="19/01/2015"></a></li>
+										<li><a href="#0" data-date="03/03/2015"></a></li>
+									</ol>
+									<span class="filling-line" aria-hidden="true"></span>
+								</div> <!-- .events -->
+							</div> <!-- End .events-wrapper -->
+							
+							<!-- Start .cd-timeline-navigation -->
+							<!-- <ul class="cd-timeline-navigation">
+								<li><a href="#0" class="prev inactive">Prev</a></li>
+								<li><a href="#0" class="next">Next</a></li>
+							</ul>  --> <!-- End .cd-timeline-navigation -->
+						</div> <!-- End .timeline -->
+					</section>
+				</div>
+			</div>
+		</div>
 	</div>
 
 	

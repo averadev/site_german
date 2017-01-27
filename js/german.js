8071f66696799e3bdecd114bfa8251bb8f7efbfb
Alberto <avera@geekbucket.com.mx>
2017-01-27 17:18:24 -0500

    Cambios German y Contacto

--- js/german.js
+++ js/german.js
@@ -2,7 +2,20 @@ var esculturas = function (){
 	var bindEvents = function(){
 		$('.button-collapse').sideNav();
 		$('.parallax').parallax();
-		$( "#topbar li:eq(4)").addClass('active'); 
+		$( "#topbar li:eq(4)").addClass('active');
+        
+        $('.smooth').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+        });
 	}
 	var onloadExec = function(){
 		bindEvents();
--- local/app/config/database.php
+++ local/app/config/database.php
@@ -54,9 +54,9 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'equipo1-pc',
 			'database'  => 'site_german',
-			'username'  => 'root',
+			'username'  => 'beto',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',

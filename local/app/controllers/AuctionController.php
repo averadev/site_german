0aca481eb69b8f524d7a62369002477bd43079b0
franciscohreyes <fhernandez@geekbucket.com.mx>
2017-01-27 10:27:28 -0500

    modificaciones modulo subasta 2_0_3_1 27-01-2017 - imagenes agregadas - modificaciones en style.css - modificaciones en style-timeline.css - modificaciones en AuctionController - modificaciones en routes.php

--- local/app/controllers/AuctionController.php
+++ local/app/controllers/AuctionController.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 
 class AuctionController extends BaseController {
 	/*
@@ -8,8 +9,44 @@ class AuctionController extends BaseController {
 	|
 	*/
 
-	public function getIndex(){
-		return View::make('subasta');
-	}
+	// public function getIndex(){
+	// 	return View::make('subasta');
+	// }
+
+	/**
+	 * Function showSubmodule
+	 */
+	public function showSubmodule () {
+		$submodule = DB::table('module', 'module.id', 'module.name')
+		->join('submodule', 'module.id', '=', 'submodule.idModule')
+		->where('module.id', '=', 2)
+		->get();
+        return View::make('subasta', array('submodule' => $submodule));
+    }
+
+    /**
+     * Function getSubmodule --> Muestra los submodulos del modulo subasta
+     */
+    public function getSubmodule () {
+    	$submodule = DB::table('module', 'module.id', 'module.name')
+		->join('submodule', 'module.id', '=', 'submodule.idModule')
+		->where('module.id', '=', 2)
+		->get();
+        return View::make('subasta', array('submodule' => $submodule));
+    }
+
+    /**
+     * Function getSectionSubmodule --> Muestra las secciones del submodulo
+     */
+    public function getSectionSubmodule ($id) {
+    	$section_submodule = DB::table('module')
+		->join('submodule', 'module.id', '=', 'submodule.idModule')
+		->join('section', 'section.idSubmodule', '=', 'submodule.id')
+		->join('xref_section_component', 'section.id', '=', 'xref_section_component.idSection')
+		->where('module.id', '=', 2)
+		->where('section.idSubmodule', '=', $id)
+		->get();
+        return View::make('subasta', array('section_submodule' => $section_submodule));
+    }
 
 }

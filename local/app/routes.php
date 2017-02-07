ae596b50669069e5bd40a72303ea1b56a01037ff
carloskf <carlos@geekbucket.com.mx>
2017-02-07 18:21:09 -0500

    se agregaron las nuevas rutas del admin

--- local/app/routes.php
+++ local/app/routes.php
@@ -5,6 +5,32 @@
 | Application Routes
 |--------------------------------------------------------------------------
 */
+
+	Route::group(array('namespace' => 'App\Controllers\Admin'), function () {
+	
+		// admin auth
+		Route::get('admin/logout', array('as' => 'admin.logout', 'uses' => 'AuthController@getLogout'));
+		Route::get('admin/login', array('as' => 'admin.login', 'uses' => 'AuthController@getLogin'));
+		Route::post('admin/login', array('as' => 'admin.login.post', 'uses' => 'AuthController@postLogin'));
+		
+	});	
+
+
+
+	/*Admin Routes*/
+	
+	Route::group(array('prefix' => 'admin', 'namespace' => 'App\Controllers\Admin', 'before' => 'auth.admin'), function () {
+	
+	    // admin dashboard
+	    Route::get('/', array('as' => 'admin.dashboard', function () {
+	
+	        return View::make('admin/dashboard')->with('active', 'home');
+	    }));	
+	
+	});
+
+
+
 	/*Servicios*/
 	Route::get('servicios/monumental', 'ServicesController@getMonumental');
 	Route::get('servicios/interiores', 'ServicesController@getInteriores');	

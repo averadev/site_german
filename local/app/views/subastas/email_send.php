236836d40d74d02f8056b1dcdd9e6a4db743b126
franciscohreyes <fhernandez@geekbucket.com.mx>
2017-02-27 18:32:55 -0500

    modificacion email proximamente - se modifico el template de correo de proximamente

--- local/app/views/subastas/email_send.php
+++ local/app/views/subastas/email_send.php
@@ -1,123 +1,342 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
-    <title>German Arzate</title>
-    <!-- CSS  -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,600i" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="{{ URL::asset('css/text-parser.css') }}">  
-    <link rel="stylesheet" href="{{ URL::asset('css/materialize.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/tabModule.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/foundation.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/style-timeline.css') }}">
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="{{ URL::asset('js/siema.js') }}"></script>
-    
-</head>
-<body>
-    <nav class="mymenu">
-        <div class="nav-wrapper gray-color">
-            <div class="container">
-                <div class="hide-on-med-and-down">
-                    <img class="brand-logo" style="margin-top: 11px; width: 160px;" src="{{ URL::asset('media/img/home/logo.png') }}" alt="logo">
-                </div>
-                <div class="hide-on-large-only">
-                    <img class="brand-logo" style="margin-top: 10px; margin-left: 35px; width: 165px;" src="{{ URL::asset('media/img/home/logo.png') }}" alt="logo">
-                </div>  
-            </div>
-        </div>
-    </nav>
+<!doctype html>
+<html>
+  <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<title>German Arzate | Próximamente</title>
+	<!-- CSS  -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,600i" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<style>
+		img {
+			border: none;
+			-ms-interpolation-mode: bicubic;
+			max-width: 100%; 
+		}
+		body {
+		background-color: #f6f6f6;
+		font-family: "Crimson Text";
+		-webkit-font-smoothing: antialiased;
+		font-size: 14px;
+		line-height: 1.4;
+		margin: 0;
+		padding: 0; 
+		-ms-text-size-adjust: 100%;
+		-webkit-text-size-adjust: 100%; 
+		}
+		table {
+			border-collapse: separate;
+			mso-table-lspace: 0pt;
+			mso-table-rspace: 0pt;
+			width: 100%; 
+		}
+		table td {
+			font-family: sans-serif;
+			font-size: 14px;
+			vertical-align: top; 
+		}
+		.body {
+			background-color: #f6f6f6;
+			width: 100%; 
+		}
+		.container {
+			display: block;
+			margin: 0 auto !important;
+			max-width: 580px;
+			padding: 10px;
+			width: 580px; 
+		}
+		.content {
+			box-sizing: border-box;
+			display: block;
+			margin: 0 auto;
+			max-width: 580px;
+			padding: 10px; 
+		}
+		.main {
+			background: #fff;
+			border-radius: 3px;
+			width: 100%;
+		}
 
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="col s12 l12">
-                    <h3 class="light center" style="text-transform: uppercase;">¡Gracias por contactarme!</h3>
-                    <div class="row">
-                        <div class="col s12 l12 center">
-                            <h4 class="center">Hola: <span><strong>{{$message}}!!</strong></span></h4>
-                            <span class="center">Tan pronto como tengamos nosotros la información con gusto te la haremos llegar a tu correo electronico</span>
-                            <p>mientras tanto te invito a que visites mi <a href="#">pagina web</a> y te enteres de mis proximas subastas.</p>
-                            <h5 class="light"><strong>Tús Datos de Contacto:</strong></h5>
-                            <span><strong>Nombre:</strong></span>: <span>Nombre Persona</span> <br>
-                            <span><strong>Correo:</strong></span>: <span>Correo Electronico</span> <br>
-                            <span><strong>Ciudad:</strong></span>: <span>Nombre Ciudad</span> <br>
-                            <span><strong>Comentario:</strong></span> <br>
-                            <span>Comentarios</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+		.wrapper {
+			box-sizing: border-box;
+			padding: 20px;
+		}
 
-    <!-- F O O T E R -->
-    <footer class="page-footer darker">
-        <div class="container" style="margin-top: 40px;">
-            <div class="row">
-                <div class="col l4 s12">
-                    <ul class="footer-info" >
-                        <li><a class="white-text" href="#!">INICIO</a></li>
-                        <li><a class="white-text" href="#!">ESCULTURAS</a></li>
-                        <li><a class="white-text" href="#!">NOTAS DE PRENSA</a></li>
-                        <li><a class="white-text" href="#!">GERMAN ARZATE</a></li>
-                        <li><a class="white-text" href="#!">CONTACTO</a></li>
-                        <li class="white-text" style="margin-top: 1em;" >SÍGUEME EN: <img class="social-button" alt="facebook" id="goface" src="{{ URL::asset('media/img/home/fb.png') }}"> <img class="social-button" alt="instagram" id="goinsta" src="{{ URL::asset('media/img/home/insta.png') }}"> <img class="social-button" alt="twitter" id="gotwitter" src="{{ URL::asset('media/img/home/twitter.png') }}"> </li>         
-                    </ul>
-                </div>
-                <div class="col l4 s12">
-                    <div class="row">
-                        <div class="col s12">
-                            <p style="margin-bottom: 1em; font-size: 14px; letter-spacing: 1px; " class="white-text"> ¡INSCRÍBITE A MI NEWSLETTER! </p>
-                        </div>
-                        <form class="myform col s11 l9 center" data-abide id="mailform">
-                        
-                            <div data-abide-error class="alert callout" style="display: none;">
-                                <p><i class="fi-alert"></i> Porfavor completa todos los campos.</p>
-                            </div>
-                            <div class="success callout" style="display: none;">
-                                <p><i class="fi-success"></i> Mensaje enviado.</p>
-                            </div>                      
-                            <input class="my-input" id="myname" required type="text" placeholder="Nombre Completo">
-                            <input class="my-input" id="myemail" pattern="valemail" required type="text" placeholder="Correo electronico">
-                            <button class="waves-effect white-border grey darken-3 waves-light btn">INSCRIBIRME</button>
-                        </form> 
+		.footer {
+			clear: both;
+			padding-top: 10px;
+			text-align: center;
+			width: 100%;
+		}
+		.footer td,
+		.footer p,
+		.footer span,
+		.footer a {
+			color: #999999;
+			font-size: 12px;
+			text-align: center;
+		}
+		h1,
+		h2,
+		h3,
+		h4 {
+			color: #000000;
+			font-family: "Crimson Text", sans-serif;
+			font-weight: 400;
+			line-height: 1.4;
+			margin: 0;
+			margin-bottom: 30px;
+		}
 
-                    </div>
-                </div>
-                <div class="col l4 s12">
-                    <ul class="footer-info">
-                        <img style="width: 7em; margin-bottom: 20px;" src="{{ URL::asset('media/img/home/logoblanco.png') }}" alt="logo footer">
-                        <li class="white-text" href="#!" >Calle Niebla No.5 S.M.18,</a></li>
-                        <li class="white-text" href="#!" >77580 Cancún, Q. Roo, México</a></li>
-                        <li class="white-text" href="#!" >Tel.: 011 521 998 106850</a></li>
-                        <li class="white-text" href="#!" >E-mail: art@germanarzate.com</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="footer-copyright">
-            <div class="container center">
-                Todos los derechos reservados German Arzate © 2017
-            </div>
-        </div>
-    </footer>   
-    <!--  Scripts-->
+		h1 {
+			font-size: 35px;
+			font-weight: 300;
+			text-align: center;
+			text-transform: capitalize;
+		}
+		p,
+		ul,
+		ol {
+			font-family: "Crimson Text", sans-serif;
+			font-size: 16px;
+			font-weight: normal;
+			margin: 0;
+			margin-bottom: 15px;
+		}
+		p li,
+		ul li,
+		ol li {
+			list-style-position: inside;
+			margin-left: 5px;
+		}
+		a {
+			color: #3498db;
+			text-decoration: underline;
+		}
 
-    <script src="{{ URL::asset('js/materialize.js') }} "></script>
-    <script src="{{ URL::asset('js/main.js') }} "></script>
-@yield('content')
-<script type="text/javascript">
-        var HOST = "{{URL::to('/')}}";
-        $.ajaxSetup({
-            headers: { 'X-CSRF-Token' : $('meta[name=_token]').attr('content') }
-        });
-</script>
-@yield('addJs')
-</body>
+		.btn {
+			box-sizing: border-box;
+			width: 100%; 
+		}
+		.btn > tbody > tr > td {
+			
+		}
+		.btn table {
+			width: auto;
+		}
+		.btn table td {
+			background-color: #ffffff;
+			border-radius: 5px;
+			text-align: center;
+		}
+		.btn a {
+			background-color: #ffffff;
+			border: solid 1px #3498db;
+			border-radius: 5px;
+			box-sizing: border-box;
+			color: #3498db;
+			cursor: pointer;
+			display: inline-block;
+			font-size: 14px;
+			font-weight: bold;
+			margin: 0;
+			padding: 12px 25px;
+			text-decoration: none;
+			text-transform: capitalize;
+		}
+		.btn-primary table td {
+			background-color: #3498db;
+		}
+		.btn-primary a {
+			background-color: #424242;
+			border-color: #424242;
+			color: #ffffff;
+		}
+		.last {
+			margin-bottom: 0;
+		}
+		.first {
+			margin-top: 0;
+		}
+		.align-center {
+			text-align: center;
+		}
+		.align-right {
+			text-align: right;
+		}
+		.align-left {
+			text-align: left;
+		}
+		.clear {
+			clear: both;
+		}
+		.mt0 {
+			margin-top: 0;
+		}
+		.mb0 {
+			margin-bottom: 0;
+		}
+		.preheader {
+			color: transparent;
+			display: none;
+			height: 0;
+			max-height: 0;
+			max-width: 0;
+			opacity: 0;
+			overflow: hidden;
+			mso-hide: all;
+			visibility: hidden;
+			width: 0;
+		}
+		.powered-by a {
+			text-decoration: none;
+		}
+		hr {
+			border: 0;
+			border-bottom: 1px solid #f6f6f6;
+			margin: 20px 0;
+		}
+		.social-button {
+			margin-bottom: 0.6em;
+			cursor: pointer;
+			margin-left: 13px;
+			width: 25px;
+		}
+
+		@media only screen and (max-width: 620px) {
+			table[class=body] h1 {
+				font-size: 28px !important;
+				margin-bottom: 10px !important;
+			}
+			table[class=body] p,
+			table[class=body] ul,
+			table[class=body] ol,
+			table[class=body] td,
+			table[class=body] span,
+			table[class=body] a {
+				font-size: 16px !important;
+			}
+			table[class=body] .wrapper,
+			table[class=body] .article {
+				padding: 10px !important;
+			}
+			table[class=body] .content {
+				padding: 0 !important; }
+			table[class=body] .container {
+				padding: 0 !important;
+				width: 100% !important;
+			}
+			table[class=body] .main {
+				border-left-width: 0 !important;
+				border-radius: 0 !important;
+				border-right-width: 0 !important;
+			}
+			table[class=body] .btn table {
+				width: 100% !important;
+			}
+			table[class=body] .btn a {
+				width: 100% !important;
+			}
+			table[class=body] .img-responsive {
+				height: auto !important;
+				max-width: 100% !important;
+				width: auto !important;
+			}
+		}
+
+		@media all {
+			.ExternalClass {
+				width: 100%;
+			}
+			.ExternalClass,
+			.ExternalClass p,
+			.ExternalClass span,
+			.ExternalClass font,
+			.ExternalClass td,
+			.ExternalClass div {
+				line-height: 100%;
+			}
+			.apple-link a {
+				color: inherit !important;
+				font-family: inherit !important;
+				font-size: inherit !important;
+				font-weight: inherit !important;
+				line-height: inherit !important;
+				text-decoration: none !important;
+			} 
+			.btn-primary table td:hover {
+				background-color: #34495e !important;
+			}
+			.btn-primary a:hover {
+				background-color: #34495e !important;
+				border-color: #34495e !important;
+			}
+		}
+	</style>
+  </head>
+	<body class="">
+		<table border="0" cellpadding="0" cellspacing="0" class="body">
+			<tr>
+				<td>&nbsp;</td>
+				<td class="container">
+					<div class="content">
+						<span class="preheader"></span>
+						<table class="main">
+						<tr>
+							<td class="wrapper">
+								<table border="0" cellpadding="0" cellspacing="0">
+									<tr>
+										<td>
+											<h2>Hola, <span><strong>{{$message}}!!</strong></span></h2>
+											<p>Tan pronto como tengamos nosotros la información con gusto te la haremos llegar a tu correo electrónico.</p>
+											<p>mientras tanto te invito a que visites mi <a href="http://hostyourgeek.com/beta/germanArzate/">pagina web</a> y te enteres de mis proximas subastas o bien puedes ir directamente en el siguiente botón.</p>
+											<table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+												<tbody>
+												<tr>
+													<td align="left">
+														<table border="0" cellpadding="0" cellspacing="0">
+															<tbody>
+															<tr>
+																<td><a href="http://hostyourgeek.com/beta/germanArzate/subasta" target="_blank">Ir a subastas</a></td>
+															</tr>
+															</tbody>
+														</table>
+													</td>
+												</tr>
+												</tbody>
+											</table>
+											<br>
+											<p>Atentamente</p>
+											<a href="http://hostyourgeek.com/beta/germanArzate/"><img src="http://hostyourgeek.com/beta/germanArzate/media/img/home/logo.png" width="165px" alt="German Arzate"></a>
+											<a href="#!" target="_blank"><img class="social-button" alt="facebook" id="goface" src="http://hostyourgeek.com/beta/germanArzate/media/img/home/fb.png"></a>
+											<a href="#!" target="_blank"><img class="social-button" alt="instagram" id="goinsta" src="http://hostyourgeek.com/beta/germanArzate/media/img/home/insta.png"></a>
+											<a href="#!" target="_blank"><img class="social-button" alt="twitter" id="gotwitter" src="http://hostyourgeek.com/beta/germanArzate/media/img/home/twitter.png"></a>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						</table>
+						<div class="footer">
+							<table border="0" cellpadding="0" cellspacing="0">
+								<tr>
+									<td class="content-block">
+										<span class="apple-link">German Arzate, Calle Niebla No. 5 S.M. 18, <br>77580 Cancún, Q. Roo, México, <br> Tel.: 011 521 998 1068505</span>
+									</td>
+								</tr>
+								<tr>
+									<td class="content-block powered-by"></td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</td>
+				<td>&nbsp;</td>
+			</tr>
+		</table>
+	</body>
 </html>
